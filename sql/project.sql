fc820d5db5bd03fa172557e0f388a0b1dee1554f
arbiterkdh <arbiterkdh@gmail.com>
2024-05-27 16:08:38 +0900

    조회 후 총 레코드 수 계산

--- sql/project.sql
+++ sql/project.sql
@@ -100,3 +100,18 @@ WHERE id % 2 = 0;
 UPDATE board
 SET member_id = 15
 WHERE id % 2 = 1;
+
+UPDATE board
+SET title   = 'abc def',
+    content = 'ghi jkl'
+WHERE id % 3 = 1;
+
+UPDATE board
+SET title   = 'mno pqr',
+    content = 'stu vwx'
+WHERE id % 3 = 0;
+
+UPDATE board
+SET title   = 'yz1 234',
+    content = '567 890'
+WHERE id % 3 = 2;
