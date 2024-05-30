cd1bc1315bd0630d95b02c8251a6f79575401b99
arbiterkdh <arbiterkdh@gmail.com>
2024-05-30 10:47:44 +0900

    댓글 작성

---
+++ src/main/java/com/prj2spring20240521/domain/Comment.java
@@ -0,0 +1,14 @@
+package com.prj2spring20240521.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Comment {
+    private Integer id;
+    private Integer boardId;
+    private Integer memberId;
+    private String comment;
+    private LocalDateTime inserted;
+}
