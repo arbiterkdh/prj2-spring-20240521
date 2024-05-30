874a41137b9d00ebf038bfedf8d4e3f67e66393f
arbiterkdh <arbiterkdh@gmail.com>
2024-05-30 11:39:09 +0900

    댓글 리스트 연습

--- src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
+++ src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
@@ -4,10 +4,9 @@ import com.prj2spring20240521.domain.Comment;
 import com.prj2spring20240521.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +19,10 @@ public class CommentController {
                            Authentication authentication) {
         System.out.println("comment = " + comment);
         service.add(comment, authentication);
+    }
+
+    @GetMapping("list/{boardId}")
+    public List<Comment> listComments(@PathVariable Integer boardId) {
+        return service.get(boardId);
     }
 }
--- src/main/java/com/prj2spring20240521/mapper/CommentMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/CommentMapper.java
@@ -3,6 +3,9 @@ package com.prj2spring20240521.mapper;
 import com.prj2spring20240521.domain.Comment;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
@@ -13,4 +16,11 @@ public interface CommentMapper {
             VALUES (#{boardId}, #{memberId}, #{comment})
             """)
     int insert(Comment comment);
+
+    @Select("""
+            SELECT *
+            FROM comment
+            WHERE board_id = #{boardId}
+            """)
+    List<Comment> selectCommentByBoardId(Integer id);
 }
