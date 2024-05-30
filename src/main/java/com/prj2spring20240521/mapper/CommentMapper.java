192bad3150faa0eec74cf00f744871801b8a0419
arbiterkdh <arbiterkdh@gmail.com>
2024-05-30 14:24:18 +0900

    댓글에 id 를 닉네임으로 변경

--- src/main/java/com/prj2spring20240521/mapper/CommentMapper.java
+++ src/main/java/com/prj2spring20240521/mapper/CommentMapper.java
@@ -18,7 +18,7 @@ public interface CommentMapper {
     int insert(Comment comment);
 
     @Select("""
-            SELECT c.id , c.board_id, c.member_id, c.comment, m.nick_name nickName
+            SELECT c.id , c.board_id, c.member_id, c.comment, c.inserted, m.nick_name nickName
             FROM comment c JOIN member m ON c.member_id = m.id
             WHERE board_id = #{boardId}
             ORDER BY id DESC
