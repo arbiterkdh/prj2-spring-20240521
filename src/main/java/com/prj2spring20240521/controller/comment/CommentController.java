f3f33c5fe3cd7bcc55b37760ec3e0b2e3bdad17c
arbiterkdh <arbiterkdh@gmail.com>
2024-05-30 11:04:57 +0900

    댓글 작성

--- src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
+++ src/main/java/com/prj2spring20240521/controller/comment/CommentController.java
@@ -1,7 +1,9 @@
 package com.prj2spring20240521.controller.comment;
 
 import com.prj2spring20240521.domain.Comment;
+import com.prj2spring20240521.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
 public class CommentController {
+    final CommentService service;
 
     @PostMapping("add")
-    public void addComment(@RequestBody Comment comment) {
+    public void addComment(@RequestBody Comment comment,
+                           Authentication authentication) {
         System.out.println("comment = " + comment);
+        service.add(comment, authentication);
     }
 }
